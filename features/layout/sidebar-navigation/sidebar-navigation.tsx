a7300174e8db01d478f62f40b1fb5f96cdc5fd6a
shakerbill <shakerbillo@yahoo.com>
2023-12-09 22:21:52 +0100

    fixed sidebar nav arrow

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -90,7 +90,9 @@ export function SidebarNavigation() {
               iconSrc="/icons/arrow-left.svg"
               isCollapsed={isSidebarCollapsed}
               onClick={() => toggleSidebar()}
-              className={styles.collapseMenuItem}
+              className={
+                isSidebarCollapsed ? styles.collapseMenuItem : undefined
+              }
             />
           </ul>
         </nav>
