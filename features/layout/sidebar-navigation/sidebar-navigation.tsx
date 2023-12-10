39737914df97dff1a8e2e7e97485506d949bac83
Emmanuel Gyan-Amoah <82492796+shakerbillo@users.noreply.github.com>
2023-12-10 14:15:45 +0100

    fixed sidebar nav arrow (#1)
    * fixed sidebar nav arrow
    
    * fixed test eror
    
    * fixed test eror
    
    * fixed sidebar collapse eror

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -20,6 +20,7 @@ export function SidebarNavigation() {
   const router = useRouter();
   const { isSidebarCollapsed, toggleSidebar } = useContext(NavigationContext);
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <div
       className={classNames(
@@ -85,12 +86,15 @@ export function SidebarNavigation() {
               isCollapsed={isSidebarCollapsed}
               onClick={() => alert("Support")}
             />
+
             <MenuItemButton
               text="Collapse"
               iconSrc="/icons/arrow-left.svg"
               isCollapsed={isSidebarCollapsed}
               onClick={() => toggleSidebar()}
-              className={styles.collapseMenuItem}
+              className={
+                isSidebarCollapsed ? styles.collapseMenuItem : undefined
+              }
             />
           </ul>
         </nav>
